d52bb6569b5e12689ba097ac571e9d21b56f0e24
DuaBilalli <duabilalli13@gmail.com>
2026-05-10 23:44:33 +0200

    Implement prediction functionality and save submission results

---
+++ src/predict.py
@@ -0,0 +1,35 @@
+import sys 
+import numpy as np 
+import pandas as pd 
+import joblib
+
+from src.prepare import prepare
+
+train, test = prepare()
+
+saved = joblib.load("models/model.pkl")
+model = saved['model']
+encoders = saved['encoders']
+
+for col, le in encoders.items():
+    test[col] = le.transform(test[col].astype(str))
+
+print("Encoding is finished")
+
+test_X = test.drop(columns=["SalePrice"], errors="ignore")
+log_predictions = model.predict(test_X)
+predictions = np.expm1(log_predictions)
+
+print(f"Parashikime te gjeneruara: {len(predictions)}") 
+print(f"\nCmimi minimal: ${predictions.min():>10,.0f}") 
+print(f"\nCmimi maksimal: ${predictions.max():>10,.0f}") 
+print(f"\nCmimi mesatar: ${predictions.mean():>10,.0f}")
+
+test_ids = pd.read_csv("data/test-house.csv")["Id"]
+submission = pd.DataFrame({
+    "Id": test_ids.values,
+    "SalePrice": predictions
+})
+
+submission.to_csv("submission.csv", index=False)
+print("submission.csv is saved")
