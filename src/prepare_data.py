7699cf87b07d1b812ace1f5febfc3c9ef95e53da
DuaBilalli <duabilalli13@gmail.com>
2026-05-10 23:01:22 +0200

    Implement data preparation function for training and testing datasets

---
+++ src/prepare_data.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+def prepare():
+    train_data = pd.read_csv("data/train-house.cv")
+    test_data = pd.read_csv("data/test-house.csv")
+
+    print(f"Train: {train_data.shape[0]} rreshta, {train_data.shape[1]} kolona")
+    print(f"Test: {test_data.shape[0]} rreshta, {train_data.shape[1]} kolona")
+
+    cols_drop = ["PoolQC", "MiscFeature", "Alley", "Fence", "Id"]
+    
+    train_data = train_data.drop(columns=cols_drop)
+    test_data = test_data.drop(columns=cols_drop)
+
+    cat_fill = [
+        "FireplaceQu", "GarageType", "GarageFinish", 
+        "GarageQual", "GarageCond", "BsmtQual", 
+        "BsmtCond", "BsmtExposure", "BsmtFinType1", 
+        "BsmtFinType2", "MasVnrType"
+    ]
+
+    for col in cat_fill:
+        if col in train_data.columns:
+            train_data[col] = train_data[col].fillna("None")
+            test_data[col] = test_data[col].fillna("None")
+
+    num_fill = [
+        "LotFrontage", "MasVnrArea", "GarageYrBlt"
+    ]
+
+    for col in num_fill:
+        if col in train_data.columns:
+            median_val = train_data[col].median()
+
+            train_data[col] = train_data[col].fillna(median_val)
+            test_data[col] = test_data[col].fillna(median_val)
+
+    for col in train_data.columns:
+        if train_data[col].isnull().sum() > 0:
+            if train_data[col].dtype == "object":
+                train_data[col] = train_data[col].fillna(train_data[col].mode()[0])
+                test_data[col] = test_data[col].fillna(test_data[col].mode()[0])
+            else:
+                train_data[col] = train_data[col].fillna(0)
+                test_data[col] = test_data[col].fillna(0)
+
+    print("\nPas pastrimit:")
+    print(f"\nTrain: {train_data.isnull().sum().sum()}")
+    print(f"\nTest: {test_data.isnull().sum().sum()}")
+
+    return train_data, test_data
+
+if __name__ == "__main__":
+    train, test = prepare()
+    print("\nData is cleaned")
